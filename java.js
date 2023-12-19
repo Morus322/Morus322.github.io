5b37ec3420b175a8a5bc2cd909994138cab9bb2c
Morus322 <154381807+Morus322@users.noreply.github.com>
2023-12-20 00:05:46 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+var adhd
+adhd = prompt ("Введіть ваше ім'я:")
+while (adhd===null || adhd.trim()=='')
+{
+    adhd=prompt("Введіть ваше ім'я:")
+}
+    alert ("Христос Рождається, "+adhd+", вітаю на моїй сторінці")
